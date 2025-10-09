962eaf719642c915afaa1a04742452d03088058f
Enryco Martins <161880607+EnrycoMartins@users.noreply.github.com>
2025-10-09 20:41:48 -0300

    telaLogin.js

---
+++ telaLogin.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('loginForm');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+});
